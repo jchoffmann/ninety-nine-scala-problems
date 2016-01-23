7fa8de4f25f3b4eed0416423b10c8e6aa16b3c97
Jens Hoffmann <jchoffmann@gmail.com>
2016-01-23 16:37:50 +0000

    Improve ArithmeticSuite (2/7) for issue #2

--- src/test/scala/ArithmeticSuite.scala
+++ src/test/scala/ArithmeticSuite.scala
@@ -1,49 +1,50 @@
 import S99Int._
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
-class ArithmeticSuite extends FunSuite {
+class ArithmeticSuite extends FunSuite with Matchers {
   test("31 determine whether a given integer number is prime") {
-    assert((1 to 20).map(_.isPrime).toList ===
-      List(false, true, true, false, true, false, true, false, false, false, true, false, true, false, false, false, true, false, true, false))
+    (1 to 20).filter(_.isPrime) shouldEqual List(2, 3, 5, 7, 11, 13, 17, 19)
   }
 
   test("32 determine the greatest common divisor of two positive integer numbers") {
-    assert(gcd(36, 63) === 9)
+    gcd(36, 63) shouldBe 9
   }
 
   test("33 determine whether two positive integer numbers are coprime") {
-    assert(35.isCoprimeTo(64))
+    35.isCoprimeTo(64) shouldBe true
   }
 
   test("34 calculate Euler's totient function phi[m]") {
-    assert(10.totient === 4)
+    10.totient shouldBe 4
   }
 
   test("35 determine the prime factors of a given positive integer") {
-    assert(315.primeFactors === List(3, 3, 5, 7))
+    315.primeFactors shouldEqual List(3, 3, 5, 7)
   }
 
   test("36 determine the prime factors of a given positive integer and their multiplicity") {
-    assert(315.primeFactorMultiplicity === List((3, 2), (5, 1), (7, 1)))
+    315.primeFactorMultiplicity shouldEqual List((3, 2), (5, 1), (7, 1))
   }
 
   test("37 calculate Euler's totient function phi[m] improved") {
-    assert(10.totientImproved === 4)
+    10.totientImproved shouldBe 4
   }
 
   test("39 construct a list of all prime numbers in a given range") {
-    assert(listPrimesInRange(7 to 31) === List(7, 11, 13, 17, 19, 23, 29, 31))
+    listPrimesInRange(7 to 31) shouldEqual List(7, 11, 13, 17, 19, 23, 29, 31)
   }
 
   test("40 find the two prime numbers that sum up to a given even integer") {
-    assert(28.goldbach ===(5, 23))
+    28.goldbach shouldEqual(5, 23)
   }
 
   test("41 list all even numbers and their Goldbach composition for a given range") {
-    assert(goldbachList(9 to 20) === Map(10 ->(3, 7), 12 ->(5, 7), 14 ->(3, 11), 16 ->(3, 13), 18 ->(5, 13), 20 ->(3, 17)))
+    goldbachList(9 to 20) shouldEqual
+      Map(10 ->(3, 7), 12 ->(5, 7), 14 ->(3, 11), 16 ->(3, 13), 18 ->(5, 13), 20 ->(3, 17))
   }
 
   test("42 list all even numbers and their Goldbach composition (each part being bigger than a given minimum) for a given range") {
-    assert(goldbachListLimited(1 to 2000, 50) === Map(992 ->(73, 919), 1382 ->(61, 1321), 1856 ->(67, 1789), 1928 ->(61, 1867)))
+    goldbachListLimited(1 to 2000, 50) shouldEqual
+      Map(992 ->(73, 919), 1382 ->(61, 1321), 1856 ->(67, 1789), 1928 ->(61, 1867))
   }
 }
