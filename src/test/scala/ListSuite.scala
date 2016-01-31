7bbc57d0cd8c1e48e1d2701442f3fe538d6a570f
Jens Hoffmann <jchoffmann@gmail.com>
2016-01-31 12:14:02 +0000

    Corrections and improvements to ListSuite

--- src/test/scala/ListSuite.scala
+++ src/test/scala/ListSuite.scala
@@ -173,42 +173,51 @@ class ListSuite extends FunSuite with Matchers with Inspectors {
     slice(3, 7, List.empty) shouldBe empty
   }
 
-  test("19 rotate a list N places to the left") {
+  test("19 rotate a list n places to the left") {
     rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) shouldEqual
       List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
   }
 
-  test("19 rotate a list N places to the right") {
+  test("19 rotate a list n places to the right") {
     rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) shouldEqual
       List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
   }
 
-  test("19 rotate an empty list N places to the right") {
+  test("19 rotating a list 0 places should leave the list unchanged") {
+    rotate(0, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) shouldEqual
+      List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+  }
+
+  test("19 rotating an empty list should result in an empty list") {
+    rotate(3, List.empty) shouldBe empty
     rotate(-2, List.empty) shouldBe empty
+    rotate(0, List.empty) shouldBe empty
   }
 
   test("20 remove the Kth element from a list") {
+    removeAt(0, List('a, 'b, 'c, 'd)) shouldEqual(List('b, 'c, 'd), 'a)
     removeAt(1, List('a, 'b, 'c, 'd)) shouldEqual(List('a, 'c, 'd), 'b)
   }
 
+  test("20 removing the Kth element from a list should throw an Exception if K is negative or too big") {
+    a[IllegalArgumentException] should be thrownBy removeAt(-1, List('a, 'b, 'c, 'd))
+    a[IllegalArgumentException] should be thrownBy removeAt(4, List('a, 'b, 'c, 'd))
+  }
+
   test("21 insert an element at a given position into a list") {
-    insertAt('new, 1, List('a, 'b, 'c, 'd)) shouldEqual List('a, 'new, 'b, 'c, 'd)
-  }
-
-  test("21 insert an element at a given position into an empty list") {
-    insertAt('new, 1, List.empty) shouldEqual List('new)
-  }
-
-  test("21 insert an element at first position into a list") {
     insertAt('new, 0, List('a, 'b, 'c, 'd)) shouldEqual List('new, 'a, 'b, 'c, 'd)
+    insertAt('new, 1, List('a, 'b, 'c, 'd)) shouldEqual List('a, 'new, 'b, 'c, 'd)
+    insertAt('new, 4, List('a, 'b, 'c, 'd)) shouldEqual List('a, 'b, 'c, 'd, 'new)
   }
 
-  test("21 insert an element at last position into a list") {
-    insertAt('new, 21, List('a, 'b, 'c, 'd)) shouldEqual List('a, 'b, 'c, 'd, 'new)
+  test("21 inserting an element at a position too small or too big should still insert at the beginning or end") {
+    insertAt('new, -1, List('a, 'b, 'c, 'd)) shouldEqual List('new, 'a, 'b, 'c, 'd)
+    insertAt('new, 5, List('a, 'b, 'c, 'd)) shouldEqual List('a, 'b, 'c, 'd, 'new)
   }
 
   test("22 create a list containing all integers within a given range") {
-    range(4, 9) shouldEqual List(4, 5, 6, 7, 8, 9)
+    range(-1, 9) shouldEqual List(-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    range(1, 1) shouldEqual List(1)
   }
 
   test("22 create a list containing all integers using an empty range") {
@@ -220,6 +229,18 @@ class ListSuite extends FunSuite with Matchers with Inspectors {
     val result = randomSelect(3, input)
     result.toSet should have size 3
     forAll(result) { r => input should contain(r) }
+  }
+
+  test("23 extracting a number of randomly selected elements from a list should throw an exception if the number is negative") {
+    an[IllegalArgumentException] should be thrownBy randomSelect(-1, List('a, 'b, 'c, 'd, 'f, 'g, 'h))
+  }
+
+  test("23 extracting zero randomly selected elements from a list should return the empty list") {
+    randomSelect(0, List('a, 'b, 'c, 'd, 'f, 'g, 'h)) shouldEqual List.empty
+  }
+
+  test("23 extract a given number of randomly selected elements from an empty list should result in an empty list") {
+    randomSelect(3, List.empty) shouldEqual List.empty
   }
 
   test("23 extract a too large number of randomly selected elements from a list") {
@@ -235,18 +256,49 @@ class ListSuite extends FunSuite with Matchers with Inspectors {
     forAll(result) { r => r should (be > 0 and be <= 49) }
   }
 
+  test("24 drawing N different random numbers from the set 1..M should throw an exception if N or M <= 0") {
+    an[IllegalArgumentException] should be thrownBy lotto(0, 49)
+    an[IllegalArgumentException] should be thrownBy lotto(6, 0)
+  }
+
+  test("24 draw N different random numbers from the set 1..M should return a permutation if N >= M") {
+    lotto(6, 6) should contain theSameElementsAs range(1, 6)
+    lotto(7, 6) should contain theSameElementsAs range(1, 6)
+  }
+
   test("25 generate a random permutation of the elements of a list") {
     val input = List('a, 'b, 'c, 'd, 'e, 'f)
     val result = randomPermute(input)
     result should contain theSameElementsAs input
   }
 
+  test("25 generate a random permutation of the empty list should yield the empty list") {
+    randomPermute(List.empty) shouldEqual List.empty
+  }
+
   test("26 generate the combinations of K distinct objects chosen from the N elements of a list") {
     val input = List('a, 'b, 'c, 'd, 'e, 'f)
     val result = combinations(3, input)
     result should have size 20
-    forAll(result) { comb => comb should have size 3 }
+    forAll(result) { comb => comb.toSet should have size 3 }
     forAll(result) { comb => forAll(comb) { elem => input should contain(elem) } }
+  }
+
+  test("26 generating the combinations of K distinct objects chosen from the N elements of a list should throw an exception if K <= 0") {
+    an[IllegalArgumentException] should be thrownBy combinations(-1, List('a, 'b, 'c, 'd, 'e, 'f))
+    an[IllegalArgumentException] should be thrownBy combinations(0, List('a, 'b, 'c, 'd, 'e, 'f))
+  }
+
+  test("26 generating the combinations of K distinct objects chosen from an empty list should yield an empty list") {
+    combinations(3, List.empty) shouldEqual List.empty
+  }
+
+  test("26 generating the combinations of K distinct objects chosen from N elements should yield an empty list if K > N") {
+    combinations(7, List('a, 'b, 'c, 'd, 'e, 'f)) shouldEqual List.empty
+  }
+
+  test("26 generate the combinations of K distinct objects chosen from the N elements of a list should contain the original for K = N") {
+    combinations(6, List('a, 'b, 'c, 'd, 'e, 'f)).head should contain theSameElementsAs List('a, 'b, 'c, 'd, 'e, 'f)
   }
 
   test("27 group the elements of a set into disjoint subsets") {
@@ -255,6 +307,28 @@ class ListSuite extends FunSuite with Matchers with Inspectors {
     result should have size 1260
     forAll(result) { r => r map (_.size) shouldEqual List(2, 3, 4) }
     forAll(result) { r => r.flatten should contain theSameElementsAs input }
+  }
+
+  test("27 grouping the elements of a an empty set into disjoint subsets should yield an empty set") {
+    group3(List.empty) shouldEqual List.empty
+  }
+
+  test("27 grouping the elements of a set into disjoint subsets of configurable size should throw an exception if the subset configuration is empty") {
+    val input = List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
+    an[IllegalArgumentException] should be thrownBy group(List.empty, input)
+  }
+
+  test("27 grouping the elements of a set into disjoint subsets of configurable size should throw an exception if the subset configuration does not match the list size") {
+    val input = List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
+    an[IllegalArgumentException] should be thrownBy group(List(2, 3, 3), input)
+    an[IllegalArgumentException] should be thrownBy group(List(2, 3, 5), input)
+    an[IllegalArgumentException] should be thrownBy group(List(2, 3, 4), List.empty)
+  }
+
+  test("27 grouping the elements of a set into disjoint subsets of configurable size should throw an exception if the subset configuration contains sizes <= 0") {
+    val input = List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
+    an[IllegalArgumentException] should be thrownBy group(List(0, 3, 6), input)
+    an[IllegalArgumentException] should be thrownBy group(List(-1, 3, 7), input)
   }
 
   test("27 group the elements of a set into disjoint subsets of configurable size") {
@@ -271,8 +345,16 @@ class ListSuite extends FunSuite with Matchers with Inspectors {
       List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l))
   }
 
+  test("28 sorting an empty list of lists according to length of sub-lists should yield an empty list") {
+    lsort(List.empty) shouldEqual List.empty
+  }
+
   test("28 sorting a list of lists according to length frequency of sub-lists") {
     lsortFreq(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) shouldEqual
       List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c), List('f, 'g, 'h), List('d, 'e), List('d, 'e), List('m, 'n))
+  }
+
+  test("28 sorting an empty list of lists according to length frequency of sub-lists should yield an empty list") {
+    lsortFreq(List.empty) shouldEqual List.empty
   }
 }
