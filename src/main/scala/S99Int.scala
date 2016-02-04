0fbfca1f8be747fd8cd9819969eff5ce57035dfd
Jens Hoffmann <jchoffmann@gmail.com>
2016-02-04 22:31:29 +0000

    Solutions up to problem 41

--- src/main/scala/S99Int.scala
+++ src/main/scala/S99Int.scala
@@ -1,37 +1,53 @@
+import S99Int._
+import S99List.encode
+
+import scala.annotation.tailrec
+
 class S99Int(val i: Int) {
   // P31
-  def isPrime: Boolean = ???
+  def isPrime: Boolean = (i > 1) && ((2 until i) forall (i % _ > 0))
 
   // P33
-  def isCoprimeTo(y: Int): Boolean = ???
+  def isCoprimeTo(y: Int): Boolean = gcd(i, y) == 1
 
   // P34
-  def totient: Int = ???
+  def totient: Int = (1 to i) count isCoprimeTo
 
   // P35
-  def primeFactors: List[Int] = ???
+  def primeFactors: List[Int] =
+    (2 to i) filter (_.isPrime) find (i % _ == 0) map (p => p +: (i / p).primeFactors) getOrElse List.empty
 
   // P36
-  def primeFactorMultiplicity: List[(Int, Int)] = ???
+  def primeFactorMultiplicity: List[(Int, Int)] = encode(i.primeFactors) map (_.swap)
 
   // P37
-  def totientImproved: Int = ???
+  def totientImproved: Int = primeFactorMultiplicity.foldLeft(1) {
+    case (accu, (p, m)) => accu * (p - 1) * Math.pow(p, m - 1).toInt
+  }
 
   // P40
-  def goldbach: (Int, Int) = ???
+  def goldbach: (Int, Int) = {
+    require(i > 2 && i % 2 == 0)
+    listPrimesInRange(2 to i).find(p => (i - p).isPrime).map(p => (p, i - p))
+      .getOrElse(throw new IllegalArgumentException)
+  }
 }
 
 object S99Int {
   implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
 
   // P32
-  def gcd(x: Int, y: Int): Int = ???
+  @tailrec
+  def gcd(x: Int, y: Int): Int = if (y == 0) x else gcd(y, x % y)
 
   // P39
-  def listPrimesInRange(fromTo: Range): List[Int] = ???
+  def listPrimesInRange(fromTo: Range): List[Int] = fromTo.filter(_.isPrime).toList
 
   // P41
-  def goldbachList(fromTo: Range): Map[Int, (Int, Int)] = ???
+  def goldbachList(fromTo: Range): Map[Int, (Int, Int)] = goldbachListLimited(fromTo, 0)
 
-  def goldbachListLimited(fromTo: Range, limit: Int): Map[Int, (Int, Int)] = ???
+  def goldbachListLimited(fromTo: Range, lower: Int): Map[Int, (Int, Int)] =
+    fromTo.filter(i => i > 2 && i % 2 == 0).map(x => (x, x.goldbach)).collect {
+      case (x, (a, b)) if a >= lower && b >= lower => x ->(a, b)
+    }.toMap
 }
