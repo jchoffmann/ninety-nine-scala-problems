cfbb4933a55d404a3edd448065add96e95e0befa
Jens Hoffmann <jchoffmann@gmail.com>
2016-02-16 00:29:49 +0000

    Solutions up to problem 70

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ninety-nine-scala-problems
 My solutions for Phil Gold's [S99: Ninety-Nine Scala Problems](http://aperiodic.net/phil/scala/s-99/).
 
-Last solved problem: 69 (end of tree exercises)
+Last solved problem: 70 (node count and string representation of multiway-trees)
--- src/main/scala/MTree.scala
+++ src/main/scala/MTree.scala
@@ -1,11 +1,13 @@
+import scala.util.parsing.combinator.RegexParsers
+
 /**
   * Exercises: http://aperiodic.net/phil/scala/s-99/#mtrees
   */
 case class MTree[+T](value: T, children: List[MTree[T]]) {
-  override def toString: String = s"M($value {" + children.map(_.toString).mkString(",") + "})"
-
   // P70
-  def nodeCount: Int = ???
+  override def toString: String = value + children.mkString + "^"
+
+  def nodeCount: Int = 1 + children.map(_.nodeCount).sum
 
   // P71
   def internalPathLength: Int = ???
@@ -21,10 +23,19 @@ object MTree {
   def apply[T](value: T) = new MTree(value, List())
 
   // P70
-  implicit def string2MTree(s: String): MTree[String] = ???
+  implicit def string2MTree(s: String): MTree[String] = MTreeParser.parseAll(MTreeParser.tree, s) match {
+    case MTreeParser.Success(tree, _) => tree
+    case MTreeParser.NoSuccess(msg, _) => throw new IllegalArgumentException(s"Cannot parse tree: $msg")
+  }
 
   def fromString(s: String): MTree[String] = string2MTree(s)
 
   // P73
   def fromLispyString(s: String): MTree[String] = ???
+}
+
+object MTreeParser extends RegexParsers {
+  def tree: Parser[MTree[String]] = label ~ rep(tree) ~ "^" ^^ { case value ~ trees ~ "^" => MTree(value, trees) }
+
+  def label: Parser[String] = "\\w".r
 }
