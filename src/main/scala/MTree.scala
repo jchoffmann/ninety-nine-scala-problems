80f23a323340c3a31f3e3c500dc1327df112fea3
Jens Hoffmann <jchoffmann@gmail.com>
2016-02-16 02:27:45 +0000

    Solutions up to problem 73

--- src/main/scala/MTree.scala
+++ src/main/scala/MTree.scala
@@ -10,32 +10,47 @@ case class MTree[+T](value: T, children: List[MTree[T]]) {
   def nodeCount: Int = 1 + children.map(_.nodeCount).sum
 
   // P71
-  def internalPathLength: Int = ???
+  def internalPathLength: Int = nodeCount - 1 + children.map(_.internalPathLength).sum
 
   // P72
-  def postorder: List[T] = ???
+  def postorder: List[T] = children.flatMap(_.postorder) ++ List(value)
 
   // P73
-  def toLispyString: String = ???
+  def toLispyString: String =
+    if (children.isEmpty) value.toString
+    else s"($value ${children.map(_.toLispyString).mkString(" ")})"
 }
 
 object MTree {
   def apply[T](value: T) = new MTree(value, List())
 
   // P70
-  implicit def string2MTree(s: String): MTree[String] = MTreeParser.parseAll(MTreeParser.tree, s) match {
-    case MTreeParser.Success(tree, _) => tree
-    case MTreeParser.NoSuccess(msg, _) => throw new IllegalArgumentException(s"Cannot parse tree: $msg")
+  implicit def string2MTree(s: String): MTree[String] = MTreeFromStringParser.parseAll(MTreeFromStringParser.tree, s) match {
+    case MTreeFromStringParser.Success(tree, _) => tree
+    case MTreeFromStringParser.NoSuccess(msg, _) => throw new IllegalArgumentException(s"Cannot parse tree: $msg")
   }
 
   def fromString(s: String): MTree[String] = string2MTree(s)
 
   // P73
-  def fromLispyString(s: String): MTree[String] = ???
+  def fromLispyString(s: String): MTree[String] = MTreeFromLispyStringParser.parseAll(MTreeFromLispyStringParser.tree, s) match {
+    case MTreeFromLispyStringParser.Success(tree, _) => tree
+    case MTreeFromLispyStringParser.NoSuccess(msg, _) => throw new IllegalArgumentException(s"Cannot parse tree: $msg")
+  }
 }
 
-object MTreeParser extends RegexParsers {
-  def tree: Parser[MTree[String]] = label ~ rep(tree) ~ "^" ^^ { case value ~ trees ~ "^" => MTree(value, trees) }
+object MTreeFromStringParser extends RegexParsers {
+  def tree: Parser[MTree[String]] = label ~ rep(tree) <~ "^" ^^ { case value ~ children => MTree(value, children) }
 
   def label: Parser[String] = "\\w".r
+}
+
+object MTreeFromLispyStringParser extends RegexParsers {
+  def tree: Parser[MTree[String]] = node | leaf
+
+  def node: Parser[MTree[String]] = ("(" ~> label) ~ (rep1(tree) <~ ")") ^^ { case value ~ children => MTree(value, children) }
+
+  def leaf: Parser[MTree[String]] = label ^^ (MTree(_))
+
+  def label: Parser[String] = "\\w+".r
 }
